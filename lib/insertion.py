087b5d5f7a0748d3619842c97dd66c599fb3acf7
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-06 18:10:57 +0100

    recharge de user_config en memoire

--- lib/insertion.py
+++ lib/insertion.py
@@ -1,9 +1,31 @@
 import json
 
-def Write_in_the_file(file,comment):
-    #load user_confi.json file and diagram in memory
+
+def open_json(file):
     with open('user_config.json') as mon_fichier:
         user_config = json.load(mon_fichier)
+    return(user_config) 
 
-    #print(user_config["user_name"])
+def open_diagram(file):
+    diagram=[]
+    try:
+        file = open(file, "r")
+        line = file.readline()
+        diagram.append(line)
+
+        while line:
+            print(line)
+            line = file.readline()
+            diagram.append(line)
+        
+        file.close()
+        return(diagram)
+    except:
+         print("Oops! file not found:the value ",file," is incorrect in the user_config.json")
     
+
+def Write_in_the_file(file,comment):
+    user_config=open_json(file)
+    diagram=open_diagram(user_config["diagram_choice"])
+    
+
