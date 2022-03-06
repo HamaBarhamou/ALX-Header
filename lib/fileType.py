15cd0223ab44844578fd9528f4314a8e9af73739
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-03-06 14:24:52 +0100

    Mise en place du package lib

--- lib/fileType.py
+++ lib/fileType.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append("/lib/fileType")
 
 def file_type(file):
     print(file)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
+from lib.fileType import file_type
 
+#from fileType import file_type
 if len(sys.argv) !=2 :
     print("Non-conforming argument.")
     print("Exemple: ./ALX-Header.sh file_path")
 else:
     file=sys.argv[1]
-    #print(file)
-    #file_type(file)
+    file_type(file)
